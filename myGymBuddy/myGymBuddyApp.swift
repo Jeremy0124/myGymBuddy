80fbae67f1877606516dfd2e8ecd5c3644c4c82a
Jeremy0124 <Jeremy.0124@yahoo.com>
2022-06-29 09:41:35 -0600

    Initial Commit

---
+++ myGymBuddy/myGymBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGymBuddyApp.swift
+//  myGymBuddy
+//
+//  Created by Jeremy Jackman on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct myGymBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
